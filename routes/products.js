b4b01a095c88153ad06c5506aee31e5e4e332301
wiseleelee <wiseleelee@gmail.com>
2021-04-25 14:56:48 +0800

    day4

--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,19 @@ const collectionId = "product";
 
 module.exports = router;
 
+router.use(asyncHandler(async function(req, res, next){
+    const headers = req.headers;
+    try{
+        if(!firebase.verifyIdToken(headers.idtoken, headers.uid)){
+            return res.json({status:"Access is prohibited"})
+        }
+        next();
+    }catch(err){
+        console.error('[Users API Middleware] : ${err}')
+        return res.json({status:"Access is prohibited"})
+    }
+}));
+
 router.get('/', asyncHandler( async (req, res, next) => {
     //Business Logic
     const products = await firebase.getCollection(collectionId)
